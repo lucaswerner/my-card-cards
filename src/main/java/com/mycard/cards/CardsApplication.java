0ae4fd735112479e3ec4b1604d6d77846d0cfddd
Lucas Werner <lucaswerner26@gmail.com>
2020-04-03 19:21:22 -0300

    auth resource server

--- src/main/java/com/mycard/cards/CardsApplication.java
+++ src/main/java/com/mycard/cards/CardsApplication.java
@@ -3,9 +3,11 @@ package com.mycard.cards;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableResourceServer
 public class CardsApplication {
 
 	public static void main(String[] args) {
