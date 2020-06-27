22d05a3f5e4d4626eb19d29c509cff38c5dcdff8
Lucas Werner <lucaswerner26@gmail.com>
2020-06-27 17:31:15 -0300

    redis config

--- src/main/java/com/mycard/cards/CardsApplication.java
+++ src/main/java/com/mycard/cards/CardsApplication.java
@@ -2,14 +2,17 @@ package com.mycard.cards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-@SpringBootApplication
 @EnableCircuitBreaker
 @EnableResourceServer
 @EnableCaching
+@SpringBootApplication(
+        exclude = {RedisRepositoriesAutoConfiguration.class}
+)
 public class CardsApplication {
 
     public static void main(String[] args) {
@@ -17,5 +20,3 @@ public class CardsApplication {
     }
 
 }
-
-
